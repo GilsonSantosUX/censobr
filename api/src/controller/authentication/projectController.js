075c78bc4456bd73c5d7eb707b74781852dc8b59
Gilson Santos <gilsonjosert@gmail.com>
2022-10-16 15:27:58 -0400

    Refactore autorization

--- api/src/controller/authentication/projectController.js
+++ api/src/controller/authentication/projectController.js
@@ -3,9 +3,16 @@ const jwt  = require('jsonwebtoken');
 
 module.exports = {
     async authenticate(req,res){
-        console.log(req.token);
-        const {message, reqStatus} = status(res.statusCode);
-        if(!req.email) return res.status(400).json({message:'Autenticação não foi valida!',status:reqStatus,data:false});
-        return res.status(200).json({message,status:reqStatus,data:true});
+        const [,token] = req.headers.authorization.split(' ');
+        jwt.verify(token,JSON.stringify(process.env.SECRET),(err,decoded)=>{
+            if(err) return res.status(401).json({message:'Token invalid!'});
+            req.idusuario = decoded.idusuario;
+            req.email = decoded.email;
+            req.papel = decoded.papel;
+            return res.status(200).json({message:"Usuário altenticado!",status:status(200).reqStatus});
+        });
+        // const {message, reqStatus} = status(res.statusCode);
+        // if(!req.email) return res.status(400).json({message:'Autenticação não foi valida!',status:reqStatus,data:false});
+        // return res.status(200).json({message,status:reqStatus,data:true});
     }
 }
